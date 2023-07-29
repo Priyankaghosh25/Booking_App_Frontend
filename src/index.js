06d21a67bed3b8c41edf4e50facc22dd8dbc2e97
Priyankaghosh25 <priyanka7890mili@gmail.com>
2023-07-29 11:38:40 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthContextProvider } from "./context/AuthContext";
+import { SearchContextProvider } from "./context/SearchContext";
+import axios from "axios";
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <AuthContextProvider>
+      <SearchContextProvider>
+        <App />
+      </SearchContextProvider>
+    </AuthContextProvider>
+  </React.StrictMode>
+);
---
+++ src/pages/bookings/Bookings.jsx
@@ -0,0 +1,38 @@
+import React, { useContext } from "react";
+import { Booking } from "../../components/Booking/Booking";
+import { Navbar } from "../../components/navbar/Navbar";
+import { AuthContext } from "../../context/AuthContext";
+import useFetch from "../../hooks/useFetch";
+import "./bookings.scss";
+
+export const Bookings = () => {
+  const { user } = useContext(AuthContext);
+
+  const { data, error, loading } = useFetch(
+    `/api/booking?id=${user._id}`
+  );
+
+  return (
+    <div className="bookings">
+      <Navbar />
+      <div className="Bcontainer">
+        <h1>Your bookings</h1>
+        {loading ? (
+          "Loading Please Wait..."
+        ) : (
+          <div className="Bwrapper">
+            {data.map((item, i) => (
+              <Booking
+                hotelId={item.hotel}
+                roomId={item.room}
+                name={user.username}
+                key={i}
+                b_id={data[i]._id}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
